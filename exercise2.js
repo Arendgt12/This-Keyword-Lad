b9270a616b4cc080df250d330639947da488a475
Jael Johnson <2023159822.ub.edu.bz>
2026-02-15 15:54:15 -0600

    Complete Exercise 2: The Data Attribute reader

---
+++ exercise2.js
@@ -0,0 +1,11 @@
+const swatches = document.querySelectorAll('.swatch');
+
+function applyDataColor() {
+    const selectedColor = this.dataset.color;
+    
+    this.style.backgroundColor = selectedColor;
+}
+
+swatches.forEach(swatch => {
+    swatch.addEventListener('click', applyDataColor);
+});
